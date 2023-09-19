d95e3932242ba755d47dc4d645a8e4c762035280
dsimplichev <dsimplichev1948@gmail.com>
2023-09-19 12:56:21 +0100

    add text/html and text/css

--- Cat-Shelter/index.js
+++ Cat-Shelter/index.js
@@ -1,9 +1,23 @@
-const http = requier("http");
-
+const http = require("http");
+const homeHtml = require("./views/home/index.js")
+const cssSite = require("./content/styles/site.js")
 const PORT = 3004;
 
 const server = http.createServer((req, res) =>{
+   const { url } = req;
 
+   if(url === "/"){
+    res.writeHead(200, {
+        "Content-Type": "text/html"
+    });
+    res.write(homeHtml)
+    
+   } else if(url === '/content/styles/site.css'){
+    res.writeHead(200,{
+        "Content-Type": "text/css"
+    });
+    res.write(cssSite);
+   }
     res.write("Hello");
     res.end();
 });
