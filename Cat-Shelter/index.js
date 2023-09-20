c75acaa8ea5f5f5329643170471fbbbad3ad6b4f
dsimplichev <dsimplichev1948@gmail.com>
2023-09-20 11:10:10 +0100

    addBreed add to project

--- Cat-Shelter/index.js
+++ Cat-Shelter/index.js
@@ -1,25 +1,31 @@
 const http = require("http");
 const homeHtml = require("./views/home/index.js")
 const cssSite = require("./content/styles/site.js")
+const addBreedHtml = require("./views/addBreed.js")
 const PORT = 3004;
 
-const server = http.createServer((req, res) =>{
-   const { url } = req;
+const server = http.createServer((req, res) => {
+    const { url } = req;
 
-   if(url === "/"){
-    res.writeHead(200, {
-        "Content-Type": "text/html"
-    });
-    res.write(homeHtml)
-    
-   } else if(url === '/content/styles/site.css'){
-    res.writeHead(200,{
-        "Content-Type": "text/css"
-    });
-    res.write(cssSite);
-   }
+    if (url === "/") {
+        res.writeHead(200, {
+            "Content-Type": "text/html"
+        });
+        res.write(homeHtml)
+
+    } else if (url === '/content/styles/site.css') {
+        res.writeHead(200, {
+            "Content-Type": "text/css"
+        });
+        res.write(cssSite);
+    } else if (url === '/cats/add-breed'){
+        res.writeHead(200,{
+            "Content-Type": "text/html"
+        });
+        res.write(addBreedHtml)
+    }
     res.write("Hello");
     res.end();
 });
 
-server.listen(PORT,  () => console.log(`Server is running on port ${PORT}`))
+server.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
