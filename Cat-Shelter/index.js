30e69a8eb9dd1039a9247b57595720b48f8b58e7
dsimplichev <dsimplichev1948@gmail.com>
2023-09-20 12:05:42 +0100

    Change cats image, descriptions, name

--- Cat-Shelter/index.js
+++ Cat-Shelter/index.js
@@ -5,32 +5,59 @@ const addBreedHtml = require("./views/addBreed.js")
 const addCatHtml = require("./views/addCat")
 const PORT = 3004;
 
+const cats = [
+    {
+        imageUrl: '',
+        name: 'Tommy',
+        breed: 'British',
+        description: 'Toomy is very cute!'
+    },
+    {
+        imageUrl: '',
+        name: 'Spot',
+        breed: 'British',
+        description: 'Spot is very cute!'
+    },
+    {
+        imageUrl: '',
+        name: 'Socks',
+        breed: 'British',
+        description: 'Socks is very cute!'
+    },
+    {
+        imageUrl: '',
+        name: 'Jerry',
+        breed: 'British',
+        description: 'Jerry is very cute!'
+    },
+]
 const server = http.createServer((req, res) => {
     const { url } = req;
 
     if (url === "/") {
+       const homeHtmlTemplate = homeHtml.replace("{{cats}}", '<h1>Cats works<h1>')
         res.writeHead(200, {
             "Content-Type": "text/html"
         });
-        res.write(homeHtml)
+        res.write(homeHtmlTemplate)
 
     } else if (url === '/content/styles/site.css') {
         res.writeHead(200, {
             "Content-Type": "text/css"
         });
         res.write(cssSite);
-    } else if (url === '/cats/add-breed'){
-        res.writeHead(200,{
+    } else if (url === '/cats/add-breed') {
+        res.writeHead(200, {
             "Content-Type": "text/html"
         });
         res.write(addBreedHtml)
     } else if (url === '/cats/add-cat') {
         res.writeHead(200, {
-            "Content-Type": "text/html"   
+            "Content-Type": "text/html"
         });
         res.write(addCatHtml)
     }
-    res.write("Hello");
+    
     res.end();
 });
 
