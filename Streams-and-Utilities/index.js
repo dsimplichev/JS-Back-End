873321ec99c8764ecce7d030798c68e11be978f3
dsimplichev <dsimplichev1948@gmail.com>
2023-10-03 17:14:36 +0100

    add uuid and set-cookie

--- Streams-and-Utilities/index.js
+++ Streams-and-Utilities/index.js
@@ -1,9 +1,12 @@
 const express = require("express");
+const { v4: uuid }= require("uuid");
 const PORT = 5004;
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send("Hello Deni!")
+    const id = uuid();
+    res.send("Hello Deni! ID:" + id);
+    req.headers("Set-Cookie" `userID = ${id}`);
 });
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`) )
