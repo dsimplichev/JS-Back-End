8e8d9d277da14fe35c3bf15b3070d310d28b7291
dsimplichev <dsimplichev1948@gmail.com>
2023-10-04 21:54:01 +0100

    fixing key value

--- Streams-and-Utilities/index.js
+++ Streams-and-Utilities/index.js
@@ -4,9 +4,20 @@ const PORT = 5004;
 const app = express();
 
 app.get('/', (req, res) => {
-    const id = uuid();
+    let id;
+    const cookie = req.headers["cookie"];
+
+    if (cookie) {
+        const [key, value] = cookie.split("=");
+        id = value;
+        console.log({ key });
+        console.log({ value });
+    } else {
+        id = uuid();
+        res.header("Set-Cookie", `userId=${id}`);
+    }
     res.send("Hello Deni! ID:" + id);
-    req.headers("Set-Cookie" `userID = ${id}`);
+    
 });
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`) )
